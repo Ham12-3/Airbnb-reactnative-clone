4b6c261907f43a27ad926fb28b3b24ce57643c33
Ham12-3 <mobolaji2309@gmail.com>
2023-11-22 20:24:16 +0000

    'update'

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,26 @@ import { View, Text } from "react-native";
 import React from "react";
 
 import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
 
 const Layout = () => {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+          fontFamily: "mon-sb",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Explore",
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default Layout;
