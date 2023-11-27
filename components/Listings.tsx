1b773315ab06a1a1f573a63349d0e391443be810
Ham12-3 <mobolaji2309@gmail.com>
2023-11-27 22:08:32 +0000

    'update'

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,10 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ListRenderItem } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import React, { useEffect, useState, useRef } from "react";
+import { defaultStyles } from "@/constants/Styles";
+import { Link } from "expo-router";
 
-const Listings = () => {
+interface Props {
+  listings: any[];
+  category: string;
+}
+
+const Listings = ({ listings: items, category }: Props) => {
+  const [loading, setLoading] = useState(false);
+
+  const listRef = useRef<FlatList>(null);
+
+  useEffect(() => {
+    console.log("reload listings: ", items.length);
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [category]);
+
+  const renderRow: ListRenderItem<any> = ({ item }) => (
+    <Link href={`/listing/${item.id}`}>Go there</Link>
+  );
+
   return (
-    <View>
-      <Text>Listings</Text>
+    <View style={defaultStyles.container}>
+      <FlatList
+        renderItem={renderRow}
+        data={loading ? [] : items}
+        ref={listRef}
+      />
     </View>
   );
 };
