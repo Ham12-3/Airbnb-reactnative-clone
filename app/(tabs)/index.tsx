0d96f81aaca5092a3dcbe813082c5db8ec2cad77
Ham12-3 <mobolaji2309@gmail.com>
2023-11-27 20:35:58 +0000

    'update'

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,23 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Link, Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
 
 const Page = () => {
+  const [category, setCategory] = useState("Tiny homes");
+  const onDataChanged = (category: string) => {
+    console.log("CHANGED_", category);
+  };
+
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Bookings</Link>
-      <Link href={"/listing/1337"}>Listing details</Link>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      <Listings />
     </View>
   );
 };
