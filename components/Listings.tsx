fb680fb4b5dde41fce9902fd8f31005248e2ff6b
Ham12-3 <mobolaji2309@gmail.com>
2023-11-27 22:24:40 +0000

    'update'

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,8 +1,16 @@
-import { View, Text, ListRenderItem } from "react-native";
+import {
+  View,
+  Text,
+  ListRenderItem,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import React, { useEffect, useState, useRef } from "react";
 import { defaultStyles } from "@/constants/Styles";
 import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Props {
   listings: any[];
@@ -24,7 +32,34 @@ const Listings = ({ listings: items, category }: Props) => {
   }, [category]);
 
   const renderRow: ListRenderItem<any> = ({ item }) => (
-    <Link href={`/listing/${item.id}`}>Go there</Link>
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <View style={styles.listing}>
+          <Image source={{ uri: item.medium_url }} style={styles.image} />
+          <TouchableOpacity
+            style={{ position: "absolute", right: 30, top: 30 }}
+          >
+            <Ionicons name="heart-outline" size={24} color={"#000"} />
+          </TouchableOpacity>
+
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "mon-sb" }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+
+          <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
 
   return (
@@ -37,5 +72,18 @@ const Listings = ({ listings: items, category }: Props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+});
 
 export default Listings;
