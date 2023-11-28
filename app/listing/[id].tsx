3f481be7410215acae7d5af85d5c6a32b2cafdda
Ham12-3 <mobolaji2309@gmail.com>
2023-11-28 20:09:44 +0000

    'update'

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -2,8 +2,13 @@ import { View, Text } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 
+import listingsData from "@/assets/data/airbnb-listings.json";
+
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+
+  const listing = (listingsData as any[]).find((item) => item.id === id);
+
   return (
     <View>
       <Text>Page</Text>
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -12,6 +12,8 @@ import { defaultStyles } from "@/constants/Styles";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
+import Animated, { FadeInRight, FadeOutLeft } from "react-native-reanimated";
+
 interface Props {
   listings: any[];
   category: string;
@@ -34,30 +36,41 @@ const Listings = ({ listings: items, category }: Props) => {
   const renderRow: ListRenderItem<any> = ({ item }) => (
     <Link href={`/listing/${item.id}`} asChild>
       <TouchableOpacity>
-        <View style={styles.listing}>
-          <Image source={{ uri: item.medium_url }} style={styles.image} />
-          <TouchableOpacity
-            style={{ position: "absolute", right: 30, top: 30 }}
-          >
-            <Ionicons name="heart-outline" size={24} color={"#000"} />
-          </TouchableOpacity>
+        <Animated.View
+          style={styles.listing}
+          entering={FadeInRight}
+          exiting={FadeOutLeft}
+        >
+          <View style={styles.listing}>
+            <Image source={{ uri: item.medium_url }} style={styles.image} />
+            <TouchableOpacity
+              style={{ position: "absolute", right: 30, top: 30 }}
+            >
+              <Ionicons name="heart-outline" size={24} color={"#000"} />
+            </TouchableOpacity>
 
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
-              {item.name}
-            </Text>
-            <View style={{ flexDirection: "row", gap: 4 }}>
-              <Ionicons name="star" size={16} />
-              <Text style={{ fontFamily: "mon-sb" }}>
-                {item.review_scores_rating / 20}
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
+                {item.name}
               </Text>
+              <View style={{ flexDirection: "row", gap: 4 }}>
+                <Ionicons name="star" size={16} />
+                <Text style={{ fontFamily: "mon-sb" }}>
+                  {item.review_scores_rating / 20}
+                </Text>
+              </View>
+            </View>
+
+            <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Text style={{ fontFamily: "mon-sb" }}>£ {item.price}</Text>
+              <Text style={{ fontFamily: "mon" }}>night</Text>
             </View>
           </View>
-
-          <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
-        </View>
+        </Animated.View>
       </TouchableOpacity>
     </Link>
   );
